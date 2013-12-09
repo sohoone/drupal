b9a759bb42fa781a4d1e71e9671b71e804e0e126
sohoone <71774559@qq.com>
2013-12-09 13:30:24 +0800

    add taxonomy

---
+++ sites/all/modules/newomg/layouts/newomg/newomg-layout.tpl.php
@@ -0,0 +1,22 @@
+
+  <div class="wrap">
+		<div class="ag-test">
+			<?php print render($page['ag-test']); ?>
+			<div class="ag1"><?php print render($page['ag1']); ?></div>
+			<div class="ag2">
+				<?php print render($page['ag2']); ?>
+				<div class="ag4"><?php print render($page['ag4']); ?></div>
+				<div class="ag5"><?php print render($page['ag5']); ?></div>
+				<div class="ag6"><?php print render($page['ag6']); ?></div>
+				<div class="ag7">
+					<?php print render($page['ag7']); ?>
+					<div class="ag8"><?php print render($page['ag8']); ?></div>
+					<div class="ag9"><?php print render($page['ag9']); ?></div>
+					<div class="ag10"><?php print render($page['ag10']); ?></div>
+				</div>
+			</div>
+			<div class="ag3"><?php print render($page['ag3']); ?></div>
+		</div>
+	</div>
+
+
